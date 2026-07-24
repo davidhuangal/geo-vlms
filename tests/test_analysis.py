70db5535c0325ea47eaffcc63cc9b5deaf3cf72b
David Huangal <dbhuangal1204@gmail.com>
2026-07-24 13:55:01 -0500

    build: attempting to cause build to fail on a scratch branch.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -4,4 +4,4 @@ from geo_vlms.analysis import normalize_text
 def test_normalize_text():
     text = "  Python 3!!   Was Released...  "
     normalized = normalize_text(text=text)
-    assert normalized == "python 3!! was released..."
+    assert normalized == "python 3!! was released... "
