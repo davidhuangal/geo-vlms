c5de0c70c461615d91a82778dff297a9e7057e31
David Huangal <dbhuangal1204@gmail.com>
2026-07-27 20:32:49 -0500

    test: added tests for counting responses with periods or spaces

--- tests/test_counting.py
+++ tests/test_counting.py
@@ -7,6 +7,14 @@ def test_counting_parse_int():
     assert parse_response("50") == 50
 
 
+def test_counting_int_with_period():
+    assert parse_response("6.") == 6
+
+
+def test_counting_int_with_spaces():
+    assert parse_response(" 10") == 10
+
+
 def test_counting_parse_commas():
     assert parse_response("1,400") == 1400
 
